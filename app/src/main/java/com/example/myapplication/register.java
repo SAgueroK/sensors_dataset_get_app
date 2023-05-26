4ba3ce3df032b80865dea06a7d4be2aad5836510
AL <codefish18@gmail.com>
2023-05-26 14:16:25 +0800

    初版1.0

--- app/src/main/java/com/example/myapplication/register.java
+++ app/src/main/java/com/example/myapplication/register.java
@@ -4,7 +4,6 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -28,7 +27,7 @@ public class register extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_register);
+        setContentView(R.layout.register);
         button_register = findViewById(R.id.button_register);
         editText_name = findViewById(R.id.user_name);
         editText_password = findViewById(R.id.password);
